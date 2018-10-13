758d84b24e8f725062f09dd315778a0db8b361fe
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:40:30 -0600

    Initial commit

---
+++ src/matrices/Matrices.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrices;
+
+/**
+ *
+ * @author Yova
+ */
+public class Matrices {
+
+    public static void main(String[] args) {
+        String nombre[] = new String[3];
+        nombre[0] = "valor1";
+        nombre[1] = "valor2";
+        nombre[2] = "valor3";
+        
+        System.out.println(nombre[2]);
+        
+    }
+    
+}
